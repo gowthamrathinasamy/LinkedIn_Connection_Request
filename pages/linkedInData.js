ef3e5e406dc4f01c9ffa47aa8d40875ec900034e
Gowtham <gowtham101910@gmail.com>
2026-09-19 22:33:33 +0530

    sendConnectionRequests updated

--- pages/linkedInData.js
+++ pages/linkedInData.js
@@ -12,14 +12,14 @@ export class linkedInData {
     //const count = await connectButton.count();
     async sendConnectionRequests(MaxRequests) {
         let i = 0;
-        for (let i = 0; i < MaxRequests; i++) {
+        for (i = 0; i < MaxRequests; i++) {
             await this.connectButton.first().click();
             if (await this.page.getByText("Your invitation to Shenhav was not sent because you have reached the weekly limit for connection invitations. Please try again next week").isVisible()) {
                 console.log("Reached weekly limit for connection invitations. Stopping further requests.");
                 break;
             }
         }
-        console.log(`Connection requests sent successfully! to ${MaxRequests} users.`);
+        console.log(`Connection requests sent successfully! to ${i} users.`);
     }
 
     async login(username, password) {
